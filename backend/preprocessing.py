86f80eb40d6452991d8ef2f7ad15ea1e4ea0c1db
rushil-dil <rushildileep@gmail.com>
2025-04-12 13:58:47 -0700

    modelwork

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -1,32 +1,32 @@
 
 
 sample_user_data = {
-    "Body Type": "overweight",
-    "Sex": "male",
-    "Diet": "omnivore",
-    "How Often Shower": "daily",
-    "Heating Energy Source": "electricity",
-    "Transport": "public transit",
-    "Vehicle Type": "hybrid",
-    "Social Activity": "moderate",
-    "Monthly Grocery Bill": 350,
-    "Frequency of Traveling by Air": "sometimes",
-    "Vehicle Monthly Distance Km": 1561,
-    "Waste Bag Size": "medium",
-    "Waste Bag Weekly Count": 2,
-    "How Long TV PC Daily Hour": 4,
-    "How Many New Clothes Monthly": 2,
-    "How Long Internet Daily Hour": 6,
-    "Energy efficiency": "Standard",
-    "Recycling": "Yes",
-    "Cooking_With": "Electricity",
+    "bodyType": "overweight",
+    "sex": "male",
+    "diet": "omnivore",
+    "shower": "daily",
+    "heatingSource": "electricity",
+    "transport": "public transit",
+    "vehicleType": "hybrid",
+    "socialActivity": "moderate",
+    "monthlyGroceryBill": 350,
+    "airTravelFrequency": "sometimes",
+    "vehicleDistance": 1561,
+    "wasteBagSize": "medium",
+    "wasteBagWeeklyCount": 2,
+    "tvPcDailyHour": 4,
+    "newClothesMonthly": 2,
+    "internetDailyHour": 6,
+    "energyEfficiency": "Standard",
+    "recycling": "Yes",
+    "cookingWith": "Electricity",
 }
 
 category_encodings = {
-    "Body Type": {'underweight': 0, 'normal': 1, 'overweight': 2, 'obese': 3},
-    "Sex": {'male': 0, 'female': 1, 'other': 2},
-    "Diet": {'vegan': 0, 'vegetarian': 1, 'omnivore': 2},
-    "How Often Shower": {'less frequently': 0, 'more frequently': 1, 'daily': 2, 'twice a day': 3},
+    "bodyType": {'underweight': 0, 'normal': 1, 'overweight': 2, 'obese': 3},
+    "sex": {'male': 0, 'female': 1, 'other': 2},
+    "diet": {'vegan': 0, 'vegetarian': 1, 'omnivore': 2},
+    "shower": {'less frequently': 0, 'more frequently': 1, 'daily': 2, 'twice a day': 3},
     "Heating Energy Source": {'electricity': 0, 'wood': 1, 'natural gas': 2, 'coal': 3},
     "Transport": {'walk/bicycle': 0, 'public': 1, 'private': 2},
     "Vehicle Type": {'none': 0, 'electric': 1, 'hybrid': 2, 'lpg': 3, 'petrol': 4, 'diesel': 5},
@@ -56,7 +56,7 @@ def processing(user_data):
             processed[key] = category_encodings[key].get(value)
         if key == 'Recycling':
             pass
-        
+
         else:
             processed[key] = value  # numeric values
     return processed
