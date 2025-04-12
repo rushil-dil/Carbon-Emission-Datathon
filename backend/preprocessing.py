42c02348400c3c7294a75f179b287701dd2cb5a6
rushil-dil <rushildileep@gmail.com>
2025-04-12 14:11:55 -0700

    preprocessing done

--- backend/preprocessing.py
+++ backend/preprocessing.py
@@ -18,8 +18,8 @@ sample_user_data = {
     "newClothesMonthly": 2,
     "internetDailyHour": 6,
     "energyEfficiency": "Standard",
-    "recycling": "Yes",
-    "cookingWith": "Electricity",
+    "recycling": ['Paper', 'Plastic', 'Glass'],
+    "cookingWith": ['Stove', 'Oven', 'Microwave'],
 }
 
 category_encodings = {
@@ -27,36 +27,24 @@ category_encodings = {
     "sex": {'male': 0, 'female': 1, 'other': 2},
     "diet": {'vegan': 0, 'vegetarian': 1, 'omnivore': 2},
     "shower": {'less frequently': 0, 'more frequently': 1, 'daily': 2, 'twice a day': 3},
-    "Heating Energy Source": {'electricity': 0, 'wood': 1, 'natural gas': 2, 'coal': 3},
-    "Transport": {'walk/bicycle': 0, 'public': 1, 'private': 2},
-    "Vehicle Type": {'none': 0, 'electric': 1, 'hybrid': 2, 'lpg': 3, 'petrol': 4, 'diesel': 5},
-    "Social Activity": {'never': 0, 'sometimes': 1, 'moderate': 2, 'often': 3},
-    "Frequency of Traveling by Air": {'never': 0, 'rarely': 1, 'sometimes': 2, 'frequently': 3, 'very frequently': 4},
-    "Waste Bag Size": {'small': 0, 'medium': 1, 'large': 2, 'extra large': 3},
-    "Energy efficiency": {'Yes': 0, 'Sometimes': 1, 'Standard': 2, 'No': 3},
-    "Recycling": {'Yes': 0, 'Sometimes': 1, 'No': 2},
-    "Cooking_With": {'Electricity': 0, 'Gas': 1, 'Wood': 2}
+    "heatingSource": {'electricity': 0, 'wood': 1, 'natural gas': 2, 'coal': 3},
+    "transport": {'walk/bicycle': 0, 'public': 1, 'private': 2},
+    "vehicleType": {'none': 0, 'electric': 1, 'hybrid': 2, 'lpg': 3, 'petrol': 4, 'diesel': 5},
+    "socialActivity": {'never': 0, 'sometimes': 1, 'moderate': 2, 'often': 3},
+    "airTravelFrequency": {'never': 0, 'rarely': 1, 'sometimes': 2, 'frequently': 3, 'very frequently': 4},
+    "wasteBagSize": {'small': 0, 'medium': 1, 'large': 2, 'extra large': 3},
+    "energyEfficiency": {'Yes': 0, 'Sometimes': 1, 'Standard': 2, 'No': 3},
 }
-
-'''
-body_type = [['underweight', 'normal', 'overweight', 'obese']] 
-shower_often = [['less frequently', 'more frequently', 'daily', 'twice a day']] 
-heating_energy = [['electricity', 'wood', 'natural gas', 'coal']] 
-transport_type = [['walk/bicycle', 'public', 'private']] 
-social_activity = [['never', 'sometimes', 'often']] 
-air_travel_freq = [['never', 'rarely', 'frequently', 'very frequently']] 
-waste_bag_size = [['small', 'medium', 'large', 'extra large']] 
-energy_efficiency = [['Yes', 'Sometimes', 'No']]
-'''
 
 def processing(user_data):
     processed = {}
     for key, value in user_data.items():
         if key in category_encodings:
             processed[key] = category_encodings[key].get(value)
-        if key == 'Recycling':
-            pass
-
+        elif key == "recycling":
+            processed[key] = 4 - len(value)
+        elif key == "cookingWith":
+            processed[key] = len(value)
         else:
             processed[key] = value  # numeric values
     return processed
