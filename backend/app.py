345b159ea16ac0cb8dc9fa3646b4a2b6a86a061a
SumanthPal <sumanth@pallamreddy.com>
2025-04-12 20:26:52 -0700

    new fix

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from preprocessing import processing
 import pandas as pd
 import joblib
+import os
 
 
 
@@ -62,7 +63,8 @@ def breakdown():
     return jsonify({'electricity': electricity, 'shopping': shopping, 'transportation': transportation, 'food': food})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
 
 
